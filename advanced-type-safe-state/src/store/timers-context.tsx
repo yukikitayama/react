97ac87422add3217846dc0e253e593107794c901
yukikitayama <yk2797@columbia.edu>
2024-07-19 11:22:26 -0700

    React Context API

--- advanced-type-safe-state/src/store/timers-context.tsx
+++ advanced-type-safe-state/src/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { type ReactNode, createContext, useContext, useReducer } from "react";
 
 type Timer = {
   name: string;
@@ -10,10 +10,45 @@ type TimersState = {
   timers: Timer[];
 };
 
+const initialState: TimersState = {
+  isRunning: true,
+  timers: []
+}
+
 type TimersContextValue = TimersState & {
-  addTimer: (timerData: Timer) => void,
-  startTimers: () => void,
-  stopTimers: () => void
+  addTimer: (timerData: Timer) => void;
+  startTimers: () => void;
+  stopTimers: () => void;
 };
 
 const TimersContext = createContext<TimersContextValue | null>(null);
+
+// hook function
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+
+  if (timersCtx === null) {
+    throw new Error('TimersContext is null');
+  }
+
+  return timersCtx;
+};
+
+type TimersContextProviderProps = {
+  children: ReactNode;
+};
+
+export default function TimersContextProvider({ children }: TimersContextProviderProps) {
+  useReducer(reducer, initialState);
+  
+  const ctx: TimersContextValue = {
+    timers: [],
+    isRunning: true,
+    addTimer(timerData) {},
+    startTimers() {},
+    stopTimers() {},
+  };
+  return (
+    <TimersContext.Provider value={ctx}>{children}</TimersContext.Provider>
+  );
+}
