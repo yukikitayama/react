9f99a069c6a6d92a835a22dc7cdfc1b5a7d0a045
yukikitayama <yk2797@columbia.edu>
2024-07-23 22:56:10 -0700

    useEffect

---
+++ side-effect-data-fetch/src/components/Timer.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState, useRef } from "react";
+
+import Container from "./UI/Container.tsx";
+import { type Timer as TimerProps } from "../store/timers-context.tsx";
+
+export default function Timer({ name, duration }: TimerProps) {
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(duration * 1000);
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  // Avoid infinity loop
+  useEffect(() => {
+    const timer = setInterval(function () {
+      setRemainingTime((prevTime) => prevTime - 50);
+    }, 50);
+    interval.current = timer;
+
+    // Cleanup function
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+
+  return (
+    <Container as="article">
+      <h2>{name}</h2>
+      <p>
+        <progress max={duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formattedRemainingTime}</p>
+    </Container>
+  );
+}
